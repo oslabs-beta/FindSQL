84bb62e29f9ac27b98f717ecb6a793c64eba0b78
June Culp <juneculp1@gmail.com>
2021-10-06 12:09:20 -0400

    setting up components + adjusted the webpack

---
+++ client/components/Columns.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Columns() {
+  return (
+    <button id = {this.props.id}>{this.props.columns}</button>
+  );
+}
---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from '/App';
+import styles from './stylesheets/style.css';
+
+render ( 
+  <App />,
+  document.getElemenyById('app')
+);
+
