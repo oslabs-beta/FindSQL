c1b672710afeadef022e444edb32b459a8b618fd
Darren <darrenc3195@gmail.com>
2021-10-13 19:22:22 -0400

    commit before merge

--- client/components/QueryGenerator.js
+++ client/components/QueryGenerator.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react';
+import queryGeneratorFunction from '../queryGeneratorFunction.js';
+
+export default function queryGenerator(props) {
+  console.log(props.queryData);
+  //useEffect: anytime a value in the queryData changes (false/true), we want to reRender the query string
+  //define a basic function that generates a simple query string based on the data this component received
 
 
-
-export default function QueryGenerator(props) {
-   return(
-    <h3></h3>
+  return(
+    <div>
+      <h3>This is the query genarator</h3>
+      <h2>{queryGeneratorFunction(props.queryData)}</h2>
+    </div>
+    
   );
 }
 
