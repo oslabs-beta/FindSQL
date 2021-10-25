873ce3c7ee733d43dbe58072c6c720490e2ec347
Carney <carneybernard23@gmail.com>
2021-10-25 12:58:15 -0400

    added logout functionality

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -32,7 +32,10 @@ export default function Sidebar(props) {
   }
   return (
     <div>
-      <h1>Hello Everyone</h1>
+      <h2>
+        Hello, <span>{props.email}</span>
+      </h2>
+      <button onClick={props.logUserOut}>Log Out</button>
       {sideTables}
     </div>
   );
