56419d3e37344f27562fde9046aa7afc303a15d2
Darren <darrenc3195@gmail.com>
2021-10-25 11:06:16 -0400

    commit before pushing changes

--- server/graphQL/server2.js
+++ server/graphQL/server2.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
-const Query = require("./resolvers/Query.js");
+const Query = require("./resolvers/query.js");
 const Mutation = require("./resolvers/Mutation");
 // 2
 const resolvers = {
